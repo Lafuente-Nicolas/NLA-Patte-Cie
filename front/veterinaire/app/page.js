a9c14bfc0c87fd2edf388cf33fbea489af339f6d
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-11-20 15:19:50 +0100

    feat: add the link for the search bar

--- front/veterinaire/app/page.js
+++ front/veterinaire/app/page.js
@@ -1,10 +1,13 @@
 import styles from "./page.module.css";
 import CarteRappel from "./components/home/carte-rappel/CarteRappel";
+import SearchBar from "./components/search/SearchBar";
+
 
 export default function Home() {
   return (
        <main>
         <CarteRappel />
+        <SearchBar />
         
        </main>
   );
