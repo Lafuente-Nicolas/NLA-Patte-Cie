8d1c33d9919879d495a6ff91796118bc3ed4454b
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-11-20 16:56:13 +0100

    feat: add the link for veterinarian

--- front/veterinaire/app/page.js
+++ front/veterinaire/app/page.js
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import CarteRappel from "./components/home/carte-rappel/CarteRappel";
 import SearchBar from "./components/search/SearchBar";
 import Category from "./components/category/Category";
-
+import Veterinarian from "./components/veterinarian/Veterinarian";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <CarteRappel />
         <SearchBar />
         <Category />
-        
+        <Veterinarian />
        </main>
   );
 }
