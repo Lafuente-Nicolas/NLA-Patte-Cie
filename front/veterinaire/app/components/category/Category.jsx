81ddf3df601aeab20200dd172aaccfa0caf21209
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-11-20 15:51:04 +0100

    add the js for the button category

--- front/veterinaire/app/components/category/Category.jsx
+++ front/veterinaire/app/components/category/Category.jsx
@@ -1,17 +1,28 @@
+"use client";
+import { useState } from "react";
 import styles from "./Category.module.css";
 
-export default function Category({ active, setActive }) {
-  const categories = ["Vaccine", "Surgery", "SPA & Treatment", "Consultation"];
+export default function Category() {
+  const categories = [
+    "Vaccine",
+    "Surgery",
+    "SPA & Treatment",
+    "Consultation"
+  ];
+
+  const [active, setActive] = useState(0); // Index du bouton actif
 
   return (
     <div className={styles.container}>
-      {categories.map((cat) => (
+      {categories.map((cat, index) => (
         <button
-          key={cat}
-          onClick={() => setActive(cat)}
-          className={active === cat ? styles.active : styles.button}
+          key={index}
+          className={`${styles.button} ${
+            active === index ? styles.active : ""
+          }`}
+          onClick={() => setActive(index)}
         >
-          {cat}
+          {cat} // Nom de la catégorie
         </button>
       ))}
     </div>
