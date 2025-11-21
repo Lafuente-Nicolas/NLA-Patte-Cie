85dca568d8bd6711c0476a331edc6be0850d7df4
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-11-21 15:43:28 +0100

    feat: add the link for the footer

--- front/veterinaire/app/page.js
+++ front/veterinaire/app/page.js
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import CarteRappel from "./components/home/carte-rappel/CarteRappel";
 import SearchBar from "./components/search/SearchBar";
 import Category from "./components/category/Category";
-import Veterinarian from "./components/veterinarian/Veterinarian";
+import Veterinarian from "./components/veterinarian/veterinarian";
 
 export default function Home() {
   return (
